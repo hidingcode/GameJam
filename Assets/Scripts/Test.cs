b679d08f5cdccd1932e19644969b5476380c52e4
hidingcode <yclim2015@gmail.com>
2022-11-02 10:30:51 +0800

    Test

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -4,15 +4,11 @@ using UnityEngine;
 
 public class Test : MonoBehaviour
 {
+    public int test1;
+
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
