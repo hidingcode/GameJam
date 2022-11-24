2de89f6187bfaf847a07eb2512f56e4af05b570a
Binkozaru <binmy2000@gmail.com>
2022-11-24 10:02:41 +0800

    Field of view, state machine

--- Assets/Codes/Scripts/FollowEnemy.cs
+++ Assets/Codes/Scripts/FollowEnemy.cs
@@ -2,18 +2,17 @@ using UnityEngine;
 
 public class FollowEnemy : MonoBehaviour
 {
-    public float speed;
-    public Transform target;
-    public float minimumDistance;
-
-    // Update is called once per frame
-    private void Update()
-    {
-        if (Vector3.Distance(transform.position, target.position) > minimumDistance){
-            transform.position = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);      
-        }else{
+    // public float speed;
+    // public Transform target;
+    // public float minimumDist;
+    // // Update is called once per frame
+    // private void Update()
+    // {
+    //     if (Vector3.Distance(transform.position, target.position) > minimumDist){
+    //         transform.position = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);   
+    //     }else{
             
 
-        }
-    }
+    //     }
+    // }
 }
