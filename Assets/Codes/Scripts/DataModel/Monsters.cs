9557117e2e89f46b577e8b55591a724e86f188be
hidingcode <yclim4635@gmail.com>
2022-11-27 13:14:34 +0800

    Customer Data Model

--- Assets/Codes/Scripts/DataModel/Monsters.cs
+++ Assets/Codes/Scripts/DataModel/Monsters.cs
@@ -10,7 +10,7 @@ namespace Codes.Scripts.DataModel
             Grounded,
             Moving
         }
-        private enum MonsterState
+        public enum MonsterState
         {
             Spawned,
             CoolDown,
@@ -20,6 +20,7 @@ namespace Codes.Scripts.DataModel
             Dead
         }
         
+        [Header("Monster Information")]
         public string monsterName = "Unnamed";
         public string description = "No description";
         public GameObject monsterPrefab;
@@ -28,7 +29,8 @@ namespace Codes.Scripts.DataModel
         public int monsterPrice;
         public bool isUnlocked;
         
-        [Header("Stats")]
+        // To prevent from modify the whole data please make a copy of the data
+        [Header("Monster Stats")]
         public float hitPoint;
         public float speedModifier;
         public float spawnCoolDown;
